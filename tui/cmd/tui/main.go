54edcd3dba4c7ab049211016f26d02e43b04ddb2
Matt DeMers <matt.s.demers@gmail.com>
2026-07-18 17:17:23 -0400

    chore: configure bubbletea go dependencies and reorg tui subdirs

--- tui/cmd/tui/main.go
+++ tui/cmd/tui/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"charm.land/bubbles/v2/spinner"
+	tea "charm.land/bubbletea/v2"
+	lipgloss "charm.land/lipgloss/v2"
+	armv1 "github.com/msdemers/roboremote/proto/gen/go/roboremote/arm/v1"
+)
+
 func main() {
-	// TODO: initialize Bubble Tea TUI
+
+	// placeholders to verify go dependencies
+	var _ = armv1.StreamEnvelope{}
+	var _ = tea.Model(nil)
+	var _ = lipgloss.Blue
+	var _ = spinner.Model{}
+	fmt.Println("presence of initial go dependencies verified")
 }
